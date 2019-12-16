b462cc50f933e3eee61428a32410e192f2bf62fc
dickanirwansyah <dickanirwansyah@gmail.com>
2019-12-16 10:05:11 +0700

    course thymeleaf

---
+++ example-thymeleaf/src/test/java/com/dicka/examplethymeleaf/ExampleThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dicka.examplethymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleThymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
